fd51129e191d150894bd6227be00c7d5067c6575
Peter Wilburn <petewilburn@hotmail.com>
2026-02-09 13:37:19 -0800

    Crane Arm removal

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -65,7 +65,7 @@ export default function Home() {
       {/* Hero Section */}
       <section className="relative h-screen flex items-center overflow-hidden bg-background blueprint-grid">
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-background/50 to-background pointer-events-none" />
-        <CraneArm /> 
+        {/* <CraneArm /> */}
 
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10 w-full">
           <div className="max-w-3xl">
